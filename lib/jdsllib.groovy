88e9ad6fcf29c3a43ad46d4ee654d7d837d2e075
ShadowUser17 <linuxforce17@gmail.com>
2021-09-24 22:18:02 +0300

    Fixed setGenericTrigger

--- lib/jdsllib.groovy
+++ lib/jdsllib.groovy
@@ -1,6 +1,5 @@
 package org.jdsllib
 
-
 void setDirs(List dirs) {
     for(dir_item in dirs) {
         folder(dir_item)
@@ -8,7 +7,7 @@ void setDirs(List dirs) {
 }
 
 
-void setActiveChoiceParam(def job_item, String param_name, String param_type, String param_script, String param_fallback) {
+void setActiveChoiceParam(def job_item, String param_name, String param_type, String param_script = "", String param_fallback = "") {
     switch(param_type) {
         case "SINGLE_SELECT":
         case  "MULTI_SELECT":
@@ -43,51 +42,60 @@ void setActiveChoiceParam(def job_item, String param_name, String param_type, St
     }
 }
 
-
+// gvars_list = [[var_key, var_val, var_exp], ...]
 void setGenericTrigger(def job_item, List gvars_list, String token_str, String token_id, String regex_text, String regex_expression) {
-    job_item.properties.pipelineTriggers.triggers {
-        genericTrigger {
-            genericVariables {
-                for(gvars_item in gvars_list) {
-                    def(var_key, var_val, var_exp) = gvars_item
+    job_item.properties {
+        pipelineTriggers {
+            triggers {
+                genericTrigger {
+                    genericVariables {
+                        for(gvars_item in gvars_list) {
+                            def(var_key, var_val, var_exp) = gvars_item
 
-                    genericVariable {
-                        key(var_key)
-                        value(var_val)
-                        expressionType("JSONPath") //Optional, defaults to JSONPath
-                        regexpFilter("") //Optional, defaults to empty string
-                        defaultValue("") //Optional, defaults to empty string
-                    } // end of genericVariable
-                }        
+                            genericVariable {
+                                key(var_key)
+                                value(var_val)
+                                if(var_exp == "") {
+                                    // Optional, defaults to JSONPath
+                                    expressionType("JSONPath")
+                                }
+                                // Optional, defaults to empty string
+                                regexpFilter("")
+                                // Optional, defaults to empty string
+                                defaultValue("")
+                            } // end of genericVariable
+                        }        
+                    }
+
+                    /*genericRequestVariables {
+                        genericRequestVariable {
+                            key("requestParameterName")
+                            regexpFilter("")
+                        }
+                    }*/
+
+                    /*genericHeaderVariables {
+                        genericHeaderVariable {
+                            key("requestHeaderName")
+                            regexpFilter("")
+                        }
+                    }*/
+
+                    token(token_str)
+                    tokenCredentialId(token_id)
+                    printContributedVariables(true)
+                    printPostContent(true)
+                    silentResponse(false)
+                    regexpFilterText(regex_text)
+                    regexpFilterExpression(regex_expression)
+                }
             }
-
-            /*genericRequestVariables {
-                genericRequestVariable {
-                    key("requestParameterName")
-                    regexpFilter("")
-                }
-            }*/
-
-            /*genericHeaderVariables {
-                genericHeaderVariable {
-                    key("requestHeaderName")
-                    regexpFilter("")
-                }
-            }*/
-
-            token(token_str)
-            tokenCredentialId(token_id)
-            printContributedVariables(true)
-            printPostContent(true)
-            silentResponse(false)
-            regexpFilterText(regex_text)
-            regexpFilterExpression(regex_expression)
         }
     }
 }
 
 
-void setPipelineScript(def job_item, String pp_script) {
+void setPipelineScript(def job_item, String pp_script = "") {
     job_item.definition {
         cps {
             script(pp_script)
@@ -95,5 +103,6 @@ void setPipelineScript(def job_item, String pp_script) {
         }
     }
 }
+
 
 return this
