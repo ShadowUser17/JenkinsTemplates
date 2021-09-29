16705b77959a53c145451f314c33ec64a143722d
ShadowUser17 <linuxforce17@gmail.com>
2021-09-29 15:47:30 +0300

    Add lib/jdsllib2.groovy (New)

---
+++ lib/jdsllib2.groovy
@@ -0,0 +1,174 @@
+void setDirs(List dirs) {
+    for(dir_item in dirs) {
+        folder(dir_item)
+    }
+}
+
+// return ["gvar_list": gvar_default, "token_str": token_str, "regex_text": regex_text, "regex_expr": regex_expr]
+Map getTriggerArgs(def token_str = "", def regex_text = "", def regex_expr = "", def gvar_list = null) {
+    def gvar_default = [:]
+
+    if(gvar_list) {
+        for(gvar_item in gvar_list) {
+            gvar_default[gvar_item.key] = gvar_item.value
+        }
+    }
+
+    def triggerArgs = [
+        "gvar_list":  gvar_default,
+        "token_str":  token_str,
+        "regex_text": regex_text,
+        "regex_expr": regex_expr
+    ]
+    
+    return triggerArgs
+}
+
+// return ["str_params": str_params_default, "ac_params": ac_params_default]
+Map getParamArgs(def str_params = null, def ac_params = null) {
+    def str_params_default = [:]
+
+    if(str_params) {
+        for(param_item in str_params) {
+            str_params_default[param_item.key] = param_item.value
+        }
+    }
+
+    def ac_params_default = [:]
+
+    if(ac_params) {
+        for(param_item in ac_params) {
+            ac_params_default[param_item.key] = param_item.value
+        }
+    }
+
+    def paramArgs = [
+        "str_params": str_params_default,
+        "ac_params":  ac_params_default
+    ]
+
+    return paramArgs
+}
+
+
+void setPipelineJob(String job_name, String pp_script = "", def trigger_args = null, def param_args = null) {
+    def job_item = pipelineJob(job_name)
+
+    job_item.definition {
+        cps {
+            if(pp_script) {
+                script(readFileFromWorkspace(job_name + "/" + pp_script))
+            } else {
+                script(pp_script)
+            }
+            sandbox()
+        }
+    } // End of base definition
+
+    if(trigger_args) {
+        job_item.properties {
+            pipelineTriggers {
+                triggers {
+                    genericTrigger {
+                        genericVariables {
+                            for(gvar_item in trigger_args.gvar_list) {
+                                def(var_val, var_exp) = gvar_item.value
+
+                                genericVariable {
+                                    key(gvar_item.key)
+                                    value(var_val)
+                                    if(!var_exp) {
+                                        // Optional, defaults to JSONPath
+                                        expressionType("JSONPath")
+                                    }
+                                    // Optional, defaults to empty string
+                                    regexpFilter("")
+                                    // Optional, defaults to empty string
+                                    defaultValue("")
+                                } // End of genericVariable
+                            } // End of for
+                        }
+
+                        token(trigger_args.token_str)
+                        tokenCredentialId("")
+                        printContributedVariables(true)
+                        printPostContent(true)
+                        silentResponse(false)
+                        regexpFilterText(trigger_args.regex_text)
+                        regexpFilterExpression(trigger_args.regex_expr)
+                    } // End of genericTrigger
+                } // End of triggers
+            } // End of pipelineTriggers
+        }
+    } // End of trigger definition
+
+    if(param_args) {
+        // sparam_list = [[param_name, param_default, param_descr], ...]
+        if(param_args.str_params) {
+            job_item.parameters {
+                for(str_item in param_args.str_params) {
+                    def(param_default, param_descr) = str_item.value
+                    stringParam(str_item.key, param_default, param_descr)
+                }
+            }
+        } // End of string params definition
+
+        // param_list = [[param_name, param_type, param_script, param_fallback], ...]
+        if(param_args.ac_params) {
+            for(ac_item in param_args.ac_params) {
+                def(param_type, param_script, param_fallback) = ac_item.value
+
+                switch(param_type) {
+                    case "SINGLE_SELECT":
+                    case  "MULTI_SELECT":
+                    case      "CHECKBOX":
+                    case         "RADIO":
+                        job_item.parameters {
+                            activeChoiceReactiveParam(ac_item.key) {
+                                choiceType(param_type)
+                                groovyScript {
+                                    if(param_script) {
+                                        script(readFileFromWorkspace(job_name + "/" + param_script))
+                                    } else {
+                                        script(param_script)
+                                    }
+
+                                    if(param_fallback) {
+                                        fallbackScript(readFileFromWorkspace(job_name + "/" + param_fallback))
+                                    } else {
+                                        fallbackScript(param_fallback)
+                                    }
+                                }
+                            }
+                        }
+                        break
+
+                    case              "TEXT_BOX":
+                    case        "FORMATTED_HTML":
+                    case "FORMATTED_HIDDEN_HTML":
+                    case          "ORDERED_LIST":
+                    case        "UNORDERED_LIST":
+                        job_item.parameters {
+                            activeChoiceReactiveReferenceParam(ac_item.key) {
+                                choiceType(param_type)
+                                groovyScript {
+                                    if(param_script) {
+                                        script(readFileFromWorkspace(job_name + "/" + param_script))
+                                    } else {
+                                        script(param_script)
+                                    }
+
+                                    if(param_fallback) {
+                                        fallbackScript(readFileFromWorkspace(job_name + "/" + param_fallback))
+                                    } else {
+                                        fallbackScript(param_fallback)
+                                    }
+                                }
+                            }
+                        }
+                        break
+                } // End of switch
+            } // End of for
+        } // End of AC definition
+    } // End of params definition
+}
